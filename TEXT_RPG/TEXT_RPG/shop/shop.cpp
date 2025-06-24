f62398a9da775ea8c639688ab7e2b687e3fdf1eb
gussen33 <gussen1221@gmail.com>
2025-06-24 15:54:00 +0900

    Update shop.cpp

--- TEXT_RPG/TEXT_RPG/shop/shop.cpp
+++ TEXT_RPG/TEXT_RPG/shop/shop.cpp
@@ -95,7 +95,7 @@ void Shop::buyItem(int index)
 	
     // subtractGold 함수 필요!
     player->addItem(item->clone());
-    player->addGold(-item->getBuyPrice());
+    player->subtractGold(item->getBuyPrice());
     cout << item->getName() << "을(를) 구매했습니다!\n";
 }
 void Shop::sellItem(int index)

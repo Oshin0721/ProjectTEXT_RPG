59fb6eef71bd190f1765ee0f8f5e4eae863e49bc
gussen33 <gussen1221@gmail.com>
2025-06-24 12:33:03 +0900

    ddd

--- TEXT_RPG/TEXT_RPG/main.cpp
+++ TEXT_RPG/TEXT_RPG/main.cpp
@@ -1,13 +1,14 @@
 ﻿#include "GameManager\GameManager.h"
 #include "Character\Character.h"
-
+#include <string>
 #include <iostream>
-
+#include "shop/shop.h"
 using namespace std;
-
 
 int main()
 {
-	
-}
 
+
+
+	return 0;
+}
